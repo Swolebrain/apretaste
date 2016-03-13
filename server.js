51c046561b72f07eb1a58a79b3bf75ef6cf3fb72
Swolebrain <vic574m@gmail.com>
2016-03-13 13:09:43 -0400

    first attempt

---
+++ server.js
@@ -0,0 +1,65 @@
+var port = 9000;
+var express = require("express");
+var app = express();
+var fs = require("fs");
+var bodyParser = require("body-parser");
+var sys = require("sys");
+var exec = require("child_process").exec;
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(function(req,res,next){
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+
+/* POST VARIABLES:
+  fileName 
+  userName
+  password
+  body
+*/
+app.post("apretaste-post-page", function(req,res){
+  var filename = req.body.fileName;
+  var userName = req.body.userName;
+  var password = req.body.password;
+  var htmlBody = req.body.body;
+  fs.readFile("/home/apretaste/"+userName+"/password", function(error, data){
+    if (!err){ //folder exists
+      if (password != data){ 
+        console.log("Intento de postear con password incorrecto: (username: ${userName}, password: ${password})");
+        res.end("Password incorrecto o alguien con ese nombre de usuario ya existe");
+      }
+      else{
+        
+      }
+    }
+    else{ //folder doesn't exist
+      exec("apretasteNewSite", function(error, stdout, stderr){
+        console.log("stdout: "+stdout);
+        console.log("stderr: "+stderr);
+        if (error){ 
+          console.log("exec error: "+error);
+          res.end("Error escribiendo su sitio: "+error);
+          return;
+        }
+        res.end("exito");
+      });
+    }
+  });
+});
+
+
+function createPage(filename, userName, htmlBody, res){
+  fs.writeFile("/home/apretaste/"+username+"/"+filename+"/index.html", htmlBody, function(err){
+    if (err){
+      console.log("Error writing file named ${filename} for ${userName}");
+      res.end("Error escribiendo el archivo de nombre ${filename} for ${userName}");
+      return;
+    } 
+    else{
+      res.end("exito");
+    }
+  });
+}
